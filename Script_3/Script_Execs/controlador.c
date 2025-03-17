94aa150dc8a01547815541fa5027f7638b01c450
Luis <luisfilipe4ever@gmail.com>
2025-03-17 21:17:21 +0000

    Controller

--- Script_3/Script_Execs/controlador.c
+++ Script_3/Script_Execs/controlador.c
@@ -2,10 +2,37 @@
 #include "mysystem.h"
 
 void controller(int N, char** commands) {
-	
-	// to do
+    int execs[N];  // Array to store execution counts
 
+    for(int i = 0; i < N; i++){
+        pid_t pid = fork();
+        if(pid == 0){
+            int count = 0;
+            int res = 1; 
+            while(res != 0){  
+                count++;
+                res = mysystem(commands[i]);  
+            }
+            _exit(count);  
+        }
+    }
+
+    // Parent waits for all children
+    for(int i = 0; i < N; i++){
+        int status = 0;
+        wait(&status);
+
+        if(WIFEXITED(status)){
+            int count = WEXITSTATUS(status);
+            execs[i] = count;
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        printf("%s %d\n", commands[i], execs[i]);
+    }
 }
+
 
 int main(int argc, char* argv[]) {
 
